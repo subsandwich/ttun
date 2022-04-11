7d01bec8bc5cb8e73aaccfdfd154485220105aa2
jake612 <jake612@live.unc.edu>
2022-04-10 21:42:23 -0400

    Created first working version

--- ttun.js
+++ ttun.js
@@ -1,18 +1,30 @@
 
-const domWalker = (domElement, executionFunction) => {
-
-
+const domWalker = (domElement) => {
+    for (let i = 0; i < domElement.children.length; i++) {
+        domWalker(domElement.children[i]);
+    }
+    manipulateNodes(domElement);
 }
 
-const strikeMich = (domElement) => {
-    domElement.innerHTML = replaceMichStrings(domElement.innerHTML)
+const manipulateNodes = (elem) => {
+    elem.childNodes.forEach(node => {
+        if (node.nodeType === 3){
+            node.nodeValue = node.nodeValue.replace(new RegExp("michigan", "i"), "ttun");
+            createStrikethroughs(node, elem);
+        }
+    });
 }
 
-const replaceMichStrings = (innerString) => {
-    innerString.replace(new RegExp("(?i)\bmichigan\b"), 'ttun');
-    //innerString.replace('M', 'M̶');
-    //innerString.replace('m', 'm̶');
-    return innerString;
+const createStrikethroughs = (textNode, parentN) => {
+    let mLoc = textNode.nodeValue.toLowerCase().indexOf('m');
+    if (mLoc !== -1) {
+        let rest = textNode.splitText(mLoc + 1);
+        let del = document.createElement('del');
+        del.appendChild(document.createTextNode(textNode.nodeValue[mLoc]));
+        parentN.insertBefore(del, rest);
+        textNode.nodeValue = textNode.nodeValue.substring(0, textNode.nodeValue.length - 1)
+        createStrikethroughs(rest, parentN);
+    }
 }
 
 
@@ -20,7 +32,7 @@ window.addEventListener('load', (event) => {
     console.log("Ready to party!");
     // Get top level node
     topElement = document.getElementsByTagName("body")[0];
-    console.log(topElement);
+    domWalker(topElement);
 
 });
 
